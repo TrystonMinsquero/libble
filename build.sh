6f915e278d651b29bc4ce196cc371940b25747d3
Tryston Minsquero <trystonminsquero@gmail.com>
2025-12-09 23:51:24 -0600

    update build.sh to only build certain parts

--- build.sh
+++ build.sh
@@ -1,7 +1,39 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-GOOS="js" GOARCH="wasm" go build -o ./site/js/main.wasm ./wasm
-go build -o ./main ./server
+
+build_wasm() {
+    GOOS="js" GOARCH="wasm" go build -o ./site/js/main.wasm ./wasm
+}
+
+build_server() {
+    go build -o ./main ./server
+}
+
+# Parse arguments
+if [ $# -eq 0 ]; then
+    # No arguments, build both
+    build_wasm
+    build_server
+else
+    for arg in "$@"; do
+        case $arg in
+            wasm)
+                build_wasm
+                ;;
+            server)
+                build_server
+                ;;
+            *)
+                echo "Unknown argument: $arg"
+                echo "Usage: $0 [wasm] [server]"
+                echo "  No arguments: build both"
+                echo "  wasm: build only WASM"
+                echo "  server: build only server"
+                exit 1
+                ;;
+        esac
+    done
+fi
 
 
