3579e5e5943d6635fb13c9e2a8a5c23f9c3f0c91
Tryston Minsquero <trystonminsquero@gmail.com>
2026-01-03 22:37:11 -0600

    extract hints to it's own file

--- wasm/game.go
+++ wasm/game.go
@@ -174,100 +174,7 @@ func setupHTML(data *SaveData, allBooks Books) {
 	hintSection := getElemByIDAs[dom.HTMLElement](doc, "hintSection")
 	skipBtn := getElemByIDAs[*dom.HTMLButtonElement](doc, "skipBtn")
 
-	type hintUI struct {
-		kind   Hint
-		elem   *dom.HTMLButtonElement
-		canUse func(g Game) bool
-		use    func(g *Game) string
-	}
-
-	const usedHintClass = "used-hint"
-	addHintElem := func(kind Hint) *dom.HTMLButtonElement {
-		// TODO: check user settings on whether to show the hint
-		e := doc.CreateElement("button")
-		e.SetTextContent(string(hintEmoji(kind)))
-
-		e.Class().Add("hint-btn")
-		e.Class().Add("game-input")
-		tooltip := hintTooltip(kind)
-		if tooltip != "" {
-			e.SetAttribute("title", tooltip)
-		}
-		hintSection.AppendChild(e)
-		button, ok := e.(*dom.HTMLButtonElement)
-		if !ok {
-			logErr("Failed to get button element for %s", string(kind))
-		}
-		return button
-	}
-	hints := []hintUI{
-		{
-			kind: HintTime,
-			elem: addHintElem(HintTime),
-			canUse: func(g Game) bool {
-				_, err := g.Book.UserData.LastReadDate()
-				if err == nil {
-					return true
-				}
-				debugPrint("Can't use time hint: %v", err)
-				return false
-			},
-			use: func(g *Game) string {
-				date, err := g.Book.UserData.LastReadDate()
-				if err != nil {
-					log(err, "Trying to use time hint but cant get date")
-					return ""
-				}
-				msg := fmt.Sprintf("You read this book in %s of %d", date.Month().String(), date.Year())
-				if !g.UsedHint(HintTime) {
-					g.UseHint(HintTime)
-				}
-				return msg
-			},
-		},
-		{
-			kind: HintSelfRating,
-			elem: addHintElem(HintSelfRating),
-			canUse: func(g Game) bool {
-				if g.Book.UserData.Stars > 0 {
-					return true
-				}
-				debugPrint("Can't use %s because there are no stars", HintSelfRating)
-				return false
-			},
-			use: func(g *Game) string {
-				msg := fmt.Sprintf("You gave this book %d stars", g.Book.UserData.Stars)
-				if !g.UsedHint(HintSelfRating) {
-					g.UseHint(HintSelfRating)
-				}
-				return msg
-			},
-		},
-		{
-			kind: HintAuthorInitial,
-			elem: addHintElem(HintAuthorInitial),
-			canUse: func(g Game) bool {
-				initials := g.Book.Book.AuthorInitials()
-				if initials != "" {
-					return true
-				}
-				debugPrint("Can't use %s. Author: %s", HintAuthorInitial, g.Book.Book.Author)
-				return initials != ""
-			},
-			use: func(g *Game) string {
-				initials := g.Book.Book.AuthorInitials()
-				if initials == "" {
-					logErr("Trying to use author initials but it's empty")
-					return ""
-				}
-				if !g.UsedHint(HintAuthorInitial) {
-					g.UseHint(HintAuthorInitial)
-				}
-				return fmt.Sprintf("The author's initials are %s", initials)
-			},
-		},
-	}
-
+	hints := createHints(hintSection)
 	updateGameProgress := func() {
 		if gameProgress == nil {
 			return
@@ -510,32 +417,6 @@ func onSkip(
 	}
 	debugPrint("The book is now %s", game.Book.Book.CleanTitle())
 	return nil
-}
-
-func hintTooltip(kind Hint) string {
-	switch kind {
-	case HintTime:
-		return "See when you last read the book"
-	case HintSelfRating:
-		return "See how many stars you gave the book"
-	case HintAuthorInitial:
-		return "See the initials of the author"
-	}
-	logErr("Hint '%s' does not have a tooltip", string(kind))
-	return ""
-}
-
-func hintEmoji(kind Hint) string {
-	switch kind {
-	case HintTime:
-		return "🕗"
-	case HintSelfRating:
-		return "⭐️"
-	case HintAuthorInitial:
-		return "🖊️"
-	}
-	logErr("Hint '%s' does not have a set emoji", string(kind))
-	return "💡"
 }
 
 func addEmojiStrip(game *Game, sb *strings.Builder) {
